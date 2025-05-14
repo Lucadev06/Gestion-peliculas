e770206a88538f2df9618bfc29943fdc84ce7633
Luca <lucasimodev01@gmail.com>
2025-05-14 19:11:12 -0300

    Primer commit: script y CSV de películas

---
+++ peliculas.py
@@ -0,0 +1,18 @@
+import csv
+
+def cargar_peliculas_csv(ruta_archivo):
+    peliculas = []
+    with open(ruta_archivo, newline='', encoding='utf-8') as archivo:
+        lector = csv.reader(archivo)
+        next(lector)  # Saltear el encabezado
+        for fila in lector:
+            titulo = fila[0]
+            genero = fila[1]
+            duracion = int(fila[2])  # Convertimos duración a número
+            peliculas.append([titulo, genero, duracion])
+    return peliculas
+
+# Ejemplo de uso
+pelis = cargar_peliculas_csv("C:/Users/aleja/Downloads/practicas Luca/Gestion-peliculas/peliculas.csv")
+for p in pelis:
+    print(p)
